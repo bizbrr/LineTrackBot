c773eab720d57d4b3e8946e20155478d07b9bb3f
Timothé Huet <timothe.huet@groupe-esigelec.org>
2022-11-10 14:15:57 +0000

    engine.h

---
+++ engine.h
@@ -0,0 +1,23 @@
+/*
+ * engine.h
+ *
+ *  Created on: 10 nov. 2022
+ *      Author: 36342511
+ */
+
+#ifndef ENGINE_H_
+#define ENGINE_H_
+
+
+void engine_forward();
+void engine_backward();
+void engine_forward();
+void engine_turn_left();
+void engine_turn_right();
+void engine_forward();
+void engine_rotate_left();
+void engine_rotate_right();
+void engine_stop();
+
+
+#endif /* ENGINE_H_ */
