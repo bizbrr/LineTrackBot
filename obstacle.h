8511e0b9884b8d69b8c07187830f46d58df12a00
Timothé Huet <timothe.huet@groupe-esigelec.org>
2022-11-10 14:16:28 +0000

    obstacle.h

---
+++ obstacle.h
@@ -0,0 +1,16 @@
+/*
+ * obstacle.h
+ *
+ *  Created on: 10 nov. 2022
+ *      Author: 36342511
+ */
+
+#ifndef OBSTACLE_H_
+#define OBSTACLE_H_
+
+
+int detect_obstacle();
+void obstacle_init();
+
+
+#endif /* OBSTACLE_H_ */
